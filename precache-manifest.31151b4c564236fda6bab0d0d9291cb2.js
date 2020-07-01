7dc40be654293ed82e3c18058fc6da0d59a53b11
Anatoly Kuzmin(tolaysha) <tolaykuzay@gmail.com>
2020-07-01 05:48:04 +0300

    Updates

---
+++ precache-manifest.31151b4c564236fda6bab0d0d9291cb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32e6182a9221b0fee138a5b62cdc77b1",
+    "url": "/videoTest/index.html"
+  },
+  {
+    "revision": "9dc9abdce4c82158c0b8",
+    "url": "/videoTest/static/css/main.260d9a48.chunk.css"
+  },
+  {
+    "revision": "a913bdebfd8ff42eb427",
+    "url": "/videoTest/static/js/2.eb1959e7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/videoTest/static/js/2.eb1959e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dc9abdce4c82158c0b8",
+    "url": "/videoTest/static/js/main.be62834b.chunk.js"
+  },
+  {
+    "revision": "eb6f14342443f08d0311",
+    "url": "/videoTest/static/js/runtime-main.9823fe61.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/videoTest/static/media/logo.5d5d9eef.svg"
+  }
+]);
