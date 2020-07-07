cdeb88d85c0eb00c8b5b7212a649c7e5fb5fa548
Anatoly Kuzmin(tolaysha) <tolaykuzay@gmail.com>
2020-07-07 09:49:20 +0300

    2160

--- src/video4.js
+++ src/video4.js
@@ -22,7 +22,7 @@ const Video4 = () => {
     navigator.webkitGetUserMedia ||
     navigator.mozGetUserMedia;
 
-  defaultsOpts.video = { facingMode: shouldFaceUser ? 'user' : 'environment', width: { ideal: 4096 }, height: { ideal: 2160 } }
+  defaultsOpts.video = { facingMode: shouldFaceUser ? 'user' : 'environment', width: { ideal: 2160 }, height: { ideal: 1024 } }
   if (typeof navigator.mediaDevices.getUserMedia === "function") {
     navigator.mediaDevices.getUserMedia(defaultsOpts)
       .then(function (_stream) {
